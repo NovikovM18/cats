d83d0ea2c63b28756967caaa6ce707302fcf35ad
NovikovM <novikov1807@gmail.com>
2022-07-06 21:41:33 +0300

    add functional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,20 @@ import { Cat } from './types';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'cats';
   API: string = 'https://api.thecatapi.com/v1/breeds';
   apiKey: string = '?api_key=71c00aa1-0cfa-4310-aa1e-89a55f657662';
-  limitLoading: number = 3;
+  limitLoading: number = 10;
   cat: Cat | undefined;
   catsOptions: Array<any> = [];
   cats: Cat[] = [];
   info: boolean = false;
   infoButtonText: string = 'show more info';
+  displayedColumns: string[] = ['nameOption', 'descriptionOption'];
+  values: number[] = []; 
+  value = '';
 
   constructor(private http: HttpClient){}
 
@@ -25,23 +29,33 @@ export class AppComponent {
     .subscribe((response) => {
       this.cats = response;
     });
+
   };
 
   setCat(cat: Cat) {
     this.cat = cat;
-    this.catsOptions = Object.entries(cat);
-    this.catsOptions.shift();
-    this.catsOptions.pop();
+    this.catsOptions = Object.entries(cat).slice(1);
   };
 
   toogleInfo() {
     this.info = !this.info;
     this.infoButtonText = this.info ? 'close description' : 'show more info';
+  };
+
+  setValues() {
+    let arr = [];
+    for (let i = 1; i <= 67; i++) {
+      arr.push(i);
+    }
+    this.values = arr;
+  };
+
+  filterList(sa: Cat[]) {
+    return sa.filter(cat => cat.name.toLowerCase().includes(this.value.toLowerCase()));
   }
 
   ngOnInit() {
     this.getCats();
-    console.log(this.cat);
-    
+    this.setValues();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-
-import {MatListModule} from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     MatListModule,
-    BrowserAnimationsModule
+    MatButtonModule,
+    MatTableModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
